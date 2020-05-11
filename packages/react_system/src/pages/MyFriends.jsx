1372ce5bfca2236c0685cc62892a023c886961c5
stockerssld <35477074+stockerssld@users.noreply.github.com>
2020-05-11 15:22:03 -0500

    Mis amigos se realizaron cambios

--- packages/react_system/src/pages/MyFriends.jsx
+++ packages/react_system/src/pages/MyFriends.jsx
@@ -11,24 +11,23 @@ const Section = styled.section`
   overflow-y: scroll;
   height: 47rem;
   width: 98%;
-  ::-webkit-scrollbar{
+  ::-webkit-scrollbar {
     width: 8px;
     height: 8px;
   }
-  ::-webkit-scrollbar-track{
+  ::-webkit-scrollbar-track {
     background: white;
     border-radius: 4px;
   }
-  :-webkit-scrollbar-track:hover{
+  :-webkit-scrollbar-track:hover {
     background: #d4d4d4;
   }
-  ::-webkit-scrollbar-track:active{
+  ::-webkit-scrollbar-track:active {
     background: #d4d4d4;
-      
   }
   ::-webkit-scrollbar-thumb:hover {
     background: blueviolet;
-  box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0.2);
+    box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0.2);
   }
 `;
 const Card = styled.div`
@@ -68,7 +67,7 @@ const CardContent = styled.div`
   }
 `;
 const CardImages = styled.div`
-  background-image: url("/images/Avatar3.png");
+  background-image: url(${(props) => props.imageBackground});
   width: 100px;
   height: 100px;
   @media screen and (max-width: 554px) {
@@ -89,324 +88,73 @@ const CardFooter = styled.footer`
   background-color: white;
   border-radius: 4px;
 `;
-const Agregar = styled.div`
-  background: red;
+const Agregar = styled.button`
+  background: #301133;
   width: 50%;
   text-align: center;
+  border: none;
+  color: white;
+  border-radius: 2px;
+  padding: 2px;
+  border: none;
+  cursor: pointer;
 `;
-const EnviarMsg = styled.div`
-  background: blue;
+const EnviarMsg = styled.button`
+  background: #332c11;
   width: 50%;
   text-align: center;
+  border: none;
+  color: white;
+  padding: 2px;
+  border: none;
+  cursor: pointer;
 `;
 const Buscar = styled.section`
-    height: 6.5rem;
-    box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
-    background-color: white;
-    color: #4a4a4a;
-    max-width: 100%;
-    position: relative;
-    display: flex;
-    border-radius: 4px;
-    width: 100%;
-    background: #292438;
-    color: white;
-    align-items: center;
-    justify-content: center;
-`
+  height: 6.5rem;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  background-color: white;
+  color: #4a4a4a;
+  max-width: 100%;
+  position: relative;
+  display: flex;
+  border-radius: 4px;
+  width: 100%;
+  background: #292438;
+  color: white;
+  align-items: center;
+  justify-content: center;
+  font-size: 25px;
+`;
+const InputBuscar = styled.input`
+  background: 0 0;
+  border: none;
+  border-bottom: 3px solid rgba(255, 255, 255, 0.2);
+  font-size: 2rem;
+  color: #fff;
+  height: "min-content";
+  width: 50%;
+  margin-left: 1rem;
+`;
 export function MyFriends() {
   const nombre = "José Antonio Padre García";
   const amigosComun = "50 amigos en común";
   return (
     <>
-    <Buscar>
-    Buscar: <input style={{ height: 'min-content'}}/>
-    </Buscar>
-    
+      <Buscar>
+        Buscar: <InputBuscar />
+      </Buscar>
+
       <Section>
-      <Card>
+       
+      {[1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2,3,4,5,6,7,].map((index, value) => (
+          <Card key={value}>
           <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
-            <CardContent>
-              {nombre}
-              <br />
-              <br />
-              {amigosComun}
-            </CardContent>
-          </CardBody>
-          <CardFooter>
-            <Agregar>Ver Perfil</Agregar>
-            <EnviarMsg>Enviar mensaje</EnviarMsg>
-          </CardFooter>
-        </Card> <Card>
-          <CardBody>
-            <CardImages />
+            <CardImages
+              imageBackground={`/images/Avatar${Math.floor(
+                Math.random() * 7 + 1
+              )}.png`}
+            />
             <CardContent>
               {nombre}
               <br />
@@ -419,6 +167,9 @@ export function MyFriends() {
             <EnviarMsg>Enviar mensaje</EnviarMsg>
           </CardFooter>
         </Card>
+            ))}
+          
+       
       </Section>
     </>
   );
